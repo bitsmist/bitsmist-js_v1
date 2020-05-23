9051d7b4ca783a1d558bc1db0a6d0ce50bca9bff
Masaki Yasutake <my17560@gmail.com>
2020-05-23 20:53:44 +0900

    Removed comments.

--- src/js/ui/custom-form.mjs
+++ src/js/ui/custom-form.mjs
@@ -24,7 +24,6 @@ export default class CustomForm extends CustomComponent
 
 	/**
      * Constructor.
-     *
      */
 	constructor()
 	{
--- src/js/ui/custom-list.mjs
+++ src/js/ui/custom-list.mjs
@@ -24,7 +24,6 @@ export default class List extends CustomComponent
 
 	/**
      * Constructor.
-     *
      */
 	constructor()
 	{
