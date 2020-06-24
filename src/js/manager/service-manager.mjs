7aaf41ed53969d62609a0ce22cae94b2a2f08ea2
Masaki Yasutake <my17560@gmail.com>
2020-06-24 12:09:36 +0900

    Modified not to use class.

--- src/js/manager/service-manager.mjs
+++ src/js/manager/service-manager.mjs
@@ -8,146 +8,141 @@
  */
 // =============================================================================
 
+import LoaderUtil from '../util/loader-util';
+
 // =============================================================================
 //	Service manager class
 // =============================================================================
 
-export default class ServiceManager
+// -----------------------------------------------------------------------------
+//  Constructor
+// -----------------------------------------------------------------------------
+
+/**
+ * Constructor.
+ */
+ export default function ServiceManager()
 {
 
-	// -------------------------------------------------------------------------
-	//  Constructor
-	// -------------------------------------------------------------------------
+	this._options = Object.assign({}, options);
+	this._app = options["app"];
+	this._services;
+	this._index;
 
-	/**
-     * Constructor.
-     *
-	 * @param	{Object}		options				Options for the component.
-     */
-	constructor(options)
+	// Load services
+	if (this._options["services"])
 	{
+		this.loadServices(this._options["services"]);
+	}
 
-		this._options = ( options ? options : {} );
-		this._app = options["app"];
-		this._services;
-		this._index;
+	return new Proxy(this, {
+		get: (target, property) => {
+			if (property in target)
+			{
+				return target[property];
+			}
+			else
+			{
+				return (...args) => {
+					return this._callMethod(property, args);
+				};
+			}
+		}
+	});
 
-		// Load services
-		if (options["services"])
+}
+
+// -----------------------------------------------------------------------------
+//  Methods
+// -----------------------------------------------------------------------------
+
+/**
+ * Add a service.
+ *
+ * @param	{String}		serviceName			Service name.
+ * @param	{Object}		options				Options for the service.
+ */
+ServiceManager.prototype.add = function(serviceName, options)
+{
+
+	options = Object.assign({}, options);
+	let className =  ("className" in options ? options["className"] : serviceName);
+	options["app"] = this._app;
+
+	let component = LoaderUtil.createObject(className, options);
+	this._services.push(component);
+	this._index[serviceName] = component;
+
+}
+
+// -----------------------------------------------------------------------------
+
+/**
+ * Add a service.
+ *
+ * @param	{Object}		settings			Service settings.
+ */
+ServiceManager.prototype.loadServices = function(settings)
+{
+
+	// Clear
+	this._services = [];
+	this._index = {};
+
+	// Add services
+	Object.keys(settings).forEach((key) => {
+		this.add(key, settings[key]);
+	});
+
+}
+
+// -----------------------------------------------------------------------------
+
+/**
+ * Get service.
+ *
+ * @param	{String}		serviceName			Service name.
+ */
+ServiceManager.prototype.getService = function(serviceName)
+{
+
+	return this._index[serviceName];
+
+}
+
+// -----------------------------------------------------------------------------
+//  Protected
+// -----------------------------------------------------------------------------
+
+/**
+ * Call service method.
+ *
+ * @param	{String}		methodName			Method name.
+ * @param	{Array}			args				Arguments to method.
+ *
+ * @return  {Promise}		Promise.
+ */
+ServiceManager.prototype._callMethod = function(methodName, args)
+{
+
+	let filter = ( args.length > 1 && typeof args[1] == "function" ? args[1] : undefined );
+
+	return new Promise((resolve, reject) => {
+		let promises = [];
+		for (let i = 0; i < this._services.length; i++)
 		{
-			this.loadServices(options["services"]);
+			if (typeof this._services[i][methodName] == "function")
+			{
+				if (!filter || filter(this._services[i]))
+				{
+					promises.push(this._services[i][methodName].apply(this._services[i], args));
+				}
+			}
 		}
 
-		return new Proxy(this, {
-			get: (target, property) => {
-				if (property in target)
-				{
-					return target[property];
-				}
-				else
-				{
-					return (...args) => {
-						return this._callMethod(property, args);
-					};
-				}
-			}
+		Promise.all(promises).then((results) => {
+			resolve(results);
 		});
-
-	}
-
-	// -------------------------------------------------------------------------
-	//  Methods
-	// -------------------------------------------------------------------------
-
-	/**
-     * Add a service.
-     *
-	 * @param	{String}		serviceName			Service name.
-	 * @param	{Object}		options				Options for the service.
-     */
-	add(serviceName, options)
-	{
-
-		let newOptions = Object.assign({}, options);
-		let className =  ("className" in options ? options["className"] : serviceName);
-		newOptions["app"] = this._app;
-
-		let component = this._app._createObject(className, newOptions); //@@@fix
-		this._services.push(component);
-		this._index[serviceName] = component;
-
-	}
-
-	// -------------------------------------------------------------------------
-
-	/**
-     * Add a service.
-     *
-	 * @param	{Object}		settings			Service settings.
-     */
-	loadServices(settings)
-	{
-
-		// Clear
-		this._services = [];
-		this._index = {};
-
-		// Add services
-		Object.keys(settings).forEach((key) => {
-			this.add(key, settings[key]);
-		});
-
-	}
-
-	// -------------------------------------------------------------------------
-
-	/**
-     * Get service.
-     *
-	 * @param	{String}		serviceName			Service name.
-     */
-	getService(serviceName)
-	{
-
-		return this._index[serviceName];
-
-	}
-
-	// -------------------------------------------------------------------------
-	//  Protected
-	// -------------------------------------------------------------------------
-
-	/**
-     * Call service method.
-     *
-	 * @param	{String}		methodName			Method name.
-	 * @param	{Array}			args				Arguments to method.
-	 *
-	 * @return  {Promise}		Promise.
-     */
-	_callMethod(methodName, args)
-	{
-
-		let filter = ( args.length > 1 && typeof args[1] == "function" ? args[1] : undefined );
-
-		return new Promise((resolve, reject) => {
-			let promises = [];
-			for (let i = 0; i < this._services.length; i++)
-			{
-				if (typeof this._services[i][methodName] == "function")
-				{
-					if (!filter || filter(this._services[i]))
-					{
-						promises.push(this._services[i][methodName].apply(this._services[i], args));
-					}
-				}
-			}
-
-			Promise.all(promises).then((results) => {
-				resolve(results);
-			});
-		});
-
-	}
+	});
 
 }
--- src/js/util/ajax-util.mjs
+++ src/js/util/ajax-util.mjs
@@ -12,130 +12,127 @@
 //	Ajax util class
 // =============================================================================
 
-export default class AjaxUtil
+export default function AjaxUtil() {}
+
+// -----------------------------------------------------------------------------
+//  Methods
+// -----------------------------------------------------------------------------
+
+/**
+ * Make an ajax request.
+ *
+ * @param	{Object}		options				Request options.
+ *
+ * @return  {Promise}		Promise.
+ */
+AjaxUtil.ajaxRequest = function(options)
 {
 
-	// -------------------------------------------------------------------------
-	//  Methods
-	// -------------------------------------------------------------------------
+	return new Promise((resolve, reject) => {
+		let url = AjaxUtil.__getOption(options, "url");
+		let method = AjaxUtil.__getOption(options, "method");
+		let data = AjaxUtil.__getOption(options, "data", "");
+		let headers = AjaxUtil.__getOption(options, "headers");
+		let xhrOptions = AjaxUtil.__getOption(options, "options");
 
-	/**
-	 * Make an ajax request.
-	 *
-	 * @param	{array}			options				Request options.
-	 *
-	 * @return  {Promise}		Promise.
-	 */
-	static ajaxRequest(options)
-	{
+		let xhr = new XMLHttpRequest();
+		xhr.open(method, url, true);
 
-		return new Promise((resolve, reject) => {
-			let url = AjaxUtil.__getOption(options, "url");
-			let method = AjaxUtil.__getOption(options, "method");
-			let data = AjaxUtil.__getOption(options, "data", "");
-			let headers = AjaxUtil.__getOption(options, "headers");
-			let xhrOptions = AjaxUtil.__getOption(options, "options");
-
-			let xhr = new XMLHttpRequest();
-			xhr.open(method, url, true);
-
-			// options
-			if (xhrOptions)
-			{
-				Object.keys(xhrOptions).forEach((option) => {
-					xhr[option] = xhrOptions[option];
-				});
-			}
-
-			// extra headers
-			if (headers)
-			{
-				Object.keys(headers).forEach((header) => {
-					xhr.setRequestHeader(header, headers[header]);
-				});
-			}
-
-			// callback (load)
-			xhr.addEventListener("load", () => {
-				if (xhr.status == 200 || xhr.status == 201)
-				{
-					resolve(xhr);
-				}
-				else
-				{
-					reject(xhr);
-				}
-			});
-
-			// callback (error)
-			xhr.addEventListener("error", () => {
-				reject(xhr);
-			});
-
-			// send
-			xhr.send(data);
-		});
-
-	}
-
-    // -------------------------------------------------------------------------
-
-	/**
-	 * Load the javascript file.
-	 *
-	 * @param	{string}		url					Javascript url.
-	 *
-	 * @return  {Promise}		Promise.
-	 */
-	static loadScript(url) {
-
-		return new Promise((resolve, reject) => {
-			let source = url;
-			let script = document.createElement('script');
-			let prior = document.getElementsByTagName('script')[0];
-			script.async = 1;
-			script.onload = script.onreadystatechange = ( _, isAbort ) => {
-				if(isAbort || !script.readyState || /loaded|complete/.test(script.readyState) ) {
-					script.onload = script.onreadystatechange = null;
-					script = undefined;
-
-					if(!isAbort) {
-						resolve();
-					}
-				}
-			};
-
-			script.src = source;
-			prior.parentNode.insertBefore(script, prior);
-		});
-
-	}
-
-	// -------------------------------------------------------------------------
-	//  Privates
-	// -------------------------------------------------------------------------
-
-	/**
-	 * Get option value. Return default value when specified key is not available.
-	 *
-	 * @param	{array}			options				Options for the component.
-	 * @param	{string}		key					Key to get.
-	 * @param	{object}		defaultValue		Value returned when key is not found.
-	 *
-	 * @return  {*}				Value.
-	 */
-	static __getOption(options, key, defaultValue)
-	{
-
-		let result = defaultValue;
-
-		if (options && (key in options))
+		// options
+		if (xhrOptions)
 		{
-			result = options[key];
+			Object.keys(xhrOptions).forEach((option) => {
+				xhr[option] = xhrOptions[option];
+			});
 		}
 
-		return result;
+		// extra headers
+		if (headers)
+		{
+			Object.keys(headers).forEach((header) => {
+				xhr.setRequestHeader(header, headers[header]);
+			});
+		}
 
+		// callback (load)
+		xhr.addEventListener("load", () => {
+			if (xhr.status == 200 || xhr.status == 201)
+			{
+				resolve(xhr);
+			}
+			else
+			{
+				reject(xhr);
+			}
+		});
+
+		// callback (error)
+		xhr.addEventListener("error", () => {
+			reject(xhr);
+		});
+
+		// send
+		xhr.send(data);
+	});
+
+}
+
+// -----------------------------------------------------------------------------
+
+/**
+ * Load the javascript file.
+ *
+ * @param	{string}		url					Javascript url.
+ *
+ * @return  {Promise}		Promise.
+ */
+AjaxUtil.loadScript = function(url) {
+
+	return new Promise((resolve, reject) => {
+		let source = url;
+		let script = document.createElement('script');
+		let prior = document.getElementsByTagName('script')[0];
+		script.async = 1;
+		script.onload = script.onreadystatechange = ( _, isAbort ) => {
+			if(isAbort || !script.readyState || /loaded|complete/.test(script.readyState) ) {
+				script.onload = script.onreadystatechange = null;
+				script = undefined;
+
+				if(!isAbort) {
+					resolve();
+				}
+			}
+		};
+
+		script.src = source;
+		prior.parentNode.insertBefore(script, prior);
+	});
+
+}
+
+// -----------------------------------------------------------------------------
+//  Privates
+// -----------------------------------------------------------------------------
+
+/**
+ * Get option value. Return default value when specified key is not available.
+ *
+ * @param	{array}			options				Options for the component.
+ * @param	{string}		key					Key to get.
+ * @param	{object}		defaultValue		Value returned when key is not found.
+ *
+ * @return  {*}				Value.
+ */
+AjaxUtil.__getOption = function(options, key, defaultValue)
+{
+
+	let result = defaultValue;
+
+	if (options && (key in options))
+	{
+		result = options[key];
 	}
+
+	return result;
 
 }
