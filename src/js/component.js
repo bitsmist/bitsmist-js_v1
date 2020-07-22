47416bb1a626a3607a6757cede3c5d441a786a61
Masaki Yasutake <my17560@gmail.com>
2020-07-22 12:50:57 +0900

    Modified: Moved event related functions to event-util.

--- src/js/component.js
+++ src/js/component.js
@@ -8,7 +8,7 @@
  */
 // =============================================================================
 
-import AjaxUtil from './util/ajax-util';
+import EventUtil from './util/event-util';
 import Globals from './globals';
 import LoaderUtil from './util/loader-util';
 
@@ -79,7 +79,10 @@ export default function Component(options)
 
 }
 
+// Inherit & Mixin
 LoaderUtil.inherit(Component, HTMLElement);
+Object.assign(Component.prototype, EventUtil);
+
 customElements.define("bm-component", Component);
 
 // -----------------------------------------------------------------------------
@@ -474,123 +477,6 @@ Component.prototype.addPlugin = function(pluginName, options)
 }
 
 // -----------------------------------------------------------------------------
-
-/**
- * Add an event handler.
- *
- * @param	{HTMLElement}	element					HTML element.
- * @param	{String}		eventName				Event name.
- * @param	{Object/String}	eventInfo				Event info.
- * @param	{Object}		options					Options passed to elements.
- * @param	{Object}		bindTo					Object which binds to handler.
- */
-Component.prototype.addEventHandler = function(element, eventName, eventInfo, options, bindTo)
-{
-
-	// Get handler
-	let handler;
-	let order = 0;
-	if ( typeof eventInfo === "object" )
-	{
-		handler = (typeof eventInfo === "object" ? eventInfo["handler"] : eventInfo);
-		order = (eventInfo["order"] ? eventInfo["order"] : order);
-	}
-	else
-	{
-		handler = eventInfo;
-	}
-
-	// Get listeners
-	let listeners = ( element._bm_detail && element._bm_detail.listeners ? element._bm_detail.listeners : {} );
-
-	// Init holder object for the element
-	if (!element._bm_detail)
-	{
-		element._bm_detail = { "component":this, "listeners":listeners };
-	}
-
-	// Add hook event handler
-	if (!listeners[eventName])
-	{
-		listeners[eventName] = [];
-		element.addEventListener(eventName, this.__callEventHandler);
-	}
-
-	listeners[eventName].push({"handler":handler, "options":options, "bind":bindTo, "order":order});
-
-	// Stable sort
-	listeners[eventName].sort((a, b) => {
-		if (a.order == b.order)		return 0;
-		else if (a.order > b.order)	return 1;
-		else 						return -1
-	});
-
-}
-
-// -----------------------------------------------------------------------------
-
-/**
- * Trigger the event.
- *
- * @param	{String}		eventName				Event name to trigger.
- * @param	{Object}		sender					Object which triggered the event.
- * @param	{Object}		options					Event parameter options.
- */
-Component.prototype.trigger = function(eventName, sender, options)
-{
-
-	options = Object.assign({}, options);
-	options["eventName"] = eventName;
-	options["sender"] = sender;
-	let e = null;
-
-	try
-	{
-		e = new CustomEvent(eventName, { detail: options });
-	}
-	catch(error)
-	{
-		e  = document.createEvent('CustomEvent');
-		e.initCustomEvent(eventName, false, false, options);
-	}
-
-	return this.__callEventHandler(e);
-
-}
-
-// -----------------------------------------------------------------------------
-
-/**
- * Trigger the HTML event.
- *
- * @param	{HTMLElement}	element					Html element.
- * @param	{String}		eventName				Event name to trigger.
- * @param	{Object}		sender					Object which triggered the event.
- * @param	{Object}		options					Event parameter options.
- */
-Component.prototype.triggerHtmlEvent = function(element, eventName, sender, options)
-{
-
-	options = Object.assign({}, options);
-	options["eventName"] = eventName;
-	options["sender"] = sender;
-	let e = null;
-
-	try
-	{
-		e = new CustomEvent(eventName, { detail: options });
-	}
-	catch(error)
-	{
-		e  = document.createEvent('CustomEvent');
-		e.initCustomEvent(eventName, false, false, options);
-	}
-
-	element.dispatchEvent(e);
-
-}
-
-// -----------------------------------------------------------------------------
 //  Callbacks
 // -----------------------------------------------------------------------------
 
@@ -808,77 +694,6 @@ Component.prototype.__autoFocus = function()
 			}
 		}
 	}
-
-}
-
-// -----------------------------------------------------------------------------
-
-/**
- * Call event handler.
- *
- * This function is registered as event listener to element.addEventListner(),
- * so "this" is HTML element which triggered the event.
- *
- * @param	{Object}		e						Event parameter.
- */
-Component.prototype.__callEventHandler = function(e)
-{
-
-	return new Promise((resolve, reject) => {
-		let listeners = ( this._bm_detail && this._bm_detail["listeners"] ? this._bm_detail["listeners"][e.type] : undefined );
-		let stopPropagation = false;
-		let chain = Promise.resolve();
-		let results = [];
-
-		if (listeners)
-		{
-			let component = this._bm_detail["component"];
-
-			for (let i = 0; i < listeners.length; i++)
-			{
-				// Get handler
-				let handler = (typeof listeners[i]["handler"] === "string" ? component[listeners[i]["handler"]] : listeners[i]["handler"] );
-
-				// Check handler
-				if (typeof handler !== "function")
-				{
-					let pluginName = (listeners[i]["bind"] ? listeners[i]["bind"]._options["className"] : "");
-					throw TypeError(`Event handler is not a function. componentName=${this.name}, pluginName=${pluginName}, eventName=${e.type}`);
-				}
-
-				// Bind handler
-				if (listeners[i]["bind"])
-				{
-					handler = handler.bind(listeners[i]["bind"]);
-				}
-
-				// Execute handler
-				chain = chain.then((result) => {
-					if (result)
-					{
-						results.push(result);
-					}
-					e.extraDetail = ( listeners[i]["options"] ? listeners[i]["options"] : {} );
-					return handler.call(component, this, e, listeners[i]["options"]);
-				});
-
-				stopPropagation = (listeners[i]["options"] && listeners[i]["options"]["stopPropagation"] ? true : stopPropagation)
-			}
-		}
-
-		if (stopPropagation)
-		{
-			e.stopPropagation();
-		}
-
-		chain.then((result) => {
-			if (result)
-			{
-				results.push(result);
-			}
-			resolve(results);
-		});
-	});
 
 }
 
