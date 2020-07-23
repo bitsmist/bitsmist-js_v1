328a131442e4062441ce312a8ecbd7660d40dc3a
Masaki Yasutake <my17560@gmail.com>
2020-07-23 19:42:14 +0900

    Added: router, settings, preferences, store.

--- src/js/globals.js
+++ src/js/globals.js
@@ -1,3 +1,17 @@
-export default {
-	"app": undefined,
-};
+import Router from "./plugin/router";
+import Store from "./plugin/store";
+
+let Globals = (function(){
+	let globals = {};
+
+	globals["app"];
+	globals["services"] = {};
+	globals["services"]["router"] = new Router();
+	globals["services"]["settings"] = new Store();
+	globals["services"]["preferences"] = new Store(null, {"loadEvent":"loadPreferences", "saveEvent":"savePreferences"});
+	globals["services"]["store"] = new Store();
+
+	return globals;
+}());
+
+export default Globals;
