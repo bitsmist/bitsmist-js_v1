788f80573709367baef4640cbf08703d1f68a290
Masaki Yasutake <my17560@gmail.com>
2020-10-23 19:07:00 +0900

    Modified to fill settings and preferences with Store.

--- src/js/globals.js
+++ src/js/globals.js
@@ -1,11 +1,13 @@
+import Store from './plugin/store';
+
 let Globals = (function(){
 	let globals = {};
 
 	globals["app"];
 	globals["components"] = {};
 	globals["classes"] = {};
-	globals["settings"];
-	globals["preferences"];
+	globals["settings"] = new Store();
+	globals["preferences"] = new Store();
 
 	return globals;
 }());
