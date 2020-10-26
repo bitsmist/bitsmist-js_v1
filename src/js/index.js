b18ebe0645f14587b1373d6d5a43927b2b558dc4
Masaki Yasutake <my17560@gmail.com>
2020-10-26 20:07:24 +0900

    Moved some components to extras.

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,30 @@
 window.BITSMIST = window.BITSMIST || {};
 window.BITSMIST.v1 = window.BITSMIST.v1 || {};
 
-// Tag loader
-import TagLoader from './tagloader';
-
-// App
-import App from './app';
-window.BITSMIST.v1.App = App;
+// Global variables
+import Globals from './globals';
+window.BITSMIST.v1.Globals = Globals;
 
 // Component
 import Component from './component';
 window.BITSMIST.v1.Component = Component;
-
-// Router
-import Router from './router';
-window.BITSMIST.v1.Router = Router;
 
 // Mixin
 import EventMixin from './mixin/event-mixin';
 import LoaderMixin from './mixin/loader-mixin';
 import WaitforMixin from './mixin/waitfor-mixin';
 
-// Plugin
-import Plugin from './plugin/plugin';
-import Store from './plugin/store';
-window.BITSMIST.v1.Plugin = Plugin;
+// Store
+import Store from './store';
 window.BITSMIST.v1.Store = Store;
 
 // Util
 import AjaxUtil from './util/ajax-util';
-import ClassUtil from './util/class-util';
-import Util from './util/util';
 window.BITSMIST.v1.AjaxUtil = AjaxUtil;
+
+import ClassUtil from './util/class-util';
 window.BITSMIST.v1.ClassUtil = ClassUtil;
+
+import Util from './util/util';
 window.BITSMIST.v1.Util = Util;
 
