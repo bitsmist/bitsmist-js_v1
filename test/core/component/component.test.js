47ee10aa63f210e2df3316a66f077502f4590a67
Masaki Yasutake <my17560@gmail.com>
2022-06-09 11:51:28 +0900

    Moved some setting test from setting.test.js to component.test.js. Added an initialization test.

--- test/core/component/component.test.js
+++ test/core/component/component.test.js
@@ -23,6 +23,19 @@ test('Inheritance Test - Component instance should be an instance of HTMLElement
 
 // -----------------------------------------------------------------------------
 
+test('Initialization Test - Component should be initialized only once', async () => {
+	document.body.innerHTML = "<div id='div1'><bar-main></bar-main></div><div id='div2'></div>";
+
+	var oldId = document.querySelector("bar-main").uniqueId;
+	document.getElementById('div2').insertAdjacentElement('afterbegin', document.querySelector('bar-main'));
+	var newId = document.querySelector("bar-main").uniqueId;
+	expect(oldId).toBe(newId);
+	console.log(document.body.innerHTML);
+
+});
+
+// -----------------------------------------------------------------------------
+
 test('State Transition Test - Does not stop when starting', async () => {
 	BarMain.prototype._getSettings = function() {
 		let settings = this.__getSettings.call(this);
@@ -66,6 +79,8 @@ test('State Transition Test - Does not stop when starting', async () => {
 	expect(barMain.testVars["eventOrder"][0]).toBe("afterStart");
 	expect(barMain.testVars["eventOrder"][1]).toBe("beforeStop");
 });
+
+// -----------------------------------------------------------------------------
 
 test('State Transition Test - Does not start when stopping', async () => {
 	BarMain.prototype._getSettings = function() {
@@ -114,6 +129,202 @@ test('State Transition Test - Does not start when stopping', async () => {
 	expect(barMain.testVars["eventOrder"][0]).toBe("beforeStart");
 	expect(barMain.testVars["eventOrder"][1]).toBe("afterStop");
 	expect(barMain.testVars["eventOrder"][2]).toBe("beforeStart");
+});
+
+// -----------------------------------------------------------------------------
+
+test('Setting Test - settings.name should be applied', async () => {
+	BarMain.prototype._getSettings = function() {
+		let settings = this.__getSettings();
+		settings["settings"]["name"] = "BarMain2";
+
+		return settings;
+	};
+
+	document.body.innerHTML = "<bar-main></bar-main>";
+	var barMain = document.querySelector("bar-main");
+
+	await BITSMIST.v1.StateOrganizer.waitFor([{"rootNode":"bar-main"}]);
+	expect(barMain.name).toBe("BarMain2");
+});
+
+// -----------------------------------------------------------------------------
+
+test('Setting Test - settings.autoSetup=true should work', async () => {
+	BarMain.prototype._getSettings = function() {
+		return this.__getSettings();
+	};
+	document.body.innerHTML = "<bar-main></bar-main>";
+	var barMain = document.querySelector("bar-main");
+
+	await BITSMIST.v1.StateOrganizer.waitFor([{"rootNode":"bar-main"}]);
+	expect(barMain.testVars["eventCalled"]["beforeSetup"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["doSetup"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["afterSetup"]).toBe(true);
+});
+
+// -----------------------------------------------------------------------------
+
+test('Setting Test - settings.autoSetup=false should work', async () => {
+	BarMain.prototype._getSettings = function() {
+		let settings = this.__getSettings.call(this);
+		settings["settings"]["autoSetup"] = false;
+
+		return settings;
+	};
+	document.body.innerHTML = "<bar-main></bar-main>";
+	var barMain = document.querySelector("bar-main");
+
+	await BITSMIST.v1.StateOrganizer.waitFor([{"rootNode":"bar-main"}]);
+	expect(barMain.testVars["eventCalled"]["beforeSetup"]).toBe(false);
+	expect(barMain.testVars["eventCalled"]["doSetup"]).toBe(false);
+	expect(barMain.testVars["eventCalled"]["afterSetup"]).toBe(false);
+});
+
+// -----------------------------------------------------------------------------
+
+test('Setting Test - settings.autoRefresh=true should work', async () => {
+	BarMain.prototype._getSettings = function() {
+		return this.__getSettings();
+	};
+	document.body.innerHTML = "<bar-main></bar-main>";
+	var barMain = document.querySelector("bar-main");
+
+	await BITSMIST.v1.StateOrganizer.waitFor([{"rootNode":"bar-main"}]);
+	expect(barMain.testVars["eventCalled"]["beforeRefresh"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["doRefresh"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["afterRefresh"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["beforeFetch"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["doTarget"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["doFetch"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["afterFetch"]).toBe(true);
+});
+
+// -----------------------------------------------------------------------------
+
+test('Setting Test - settings.autoRefresh=false should work', async () => {
+	BarMain.prototype._getSettings = function() {
+		let settings = this.__getSettings.call(this);
+		settings["settings"]["autoRefresh"] = false;
+
+		return settings;
+	};
+	document.body.innerHTML = "<bar-main></bar-main>";
+	var barMain = document.querySelector("bar-main");
+
+	await BITSMIST.v1.StateOrganizer.waitFor([{"rootNode":"bar-main"}]);
+	expect(barMain.testVars["eventCalled"]["beforeRefresh"]).toBe(false);
+	expect(barMain.testVars["eventCalled"]["doRefresh"]).toBe(false);
+	expect(barMain.testVars["eventCalled"]["afterRefresh"]).toBe(false);
+	expect(barMain.testVars["eventCalled"]["beforeFetch"]).toBe(false);
+	expect(barMain.testVars["eventCalled"]["doTarget"]).toBe(false);
+	expect(barMain.testVars["eventCalled"]["doFetch"]).toBe(false);
+	expect(barMain.testVars["eventCalled"]["afterFetch"]).toBe(false);
+});
+
+// -----------------------------------------------------------------------------
+
+test('Setting Test - settings.autoFetch=true should work', async () => {
+	BarMain.prototype._getSettings = function() {
+		return this.__getSettings();
+	};
+	document.body.innerHTML = "<bar-main></bar-main>";
+	var barMain = document.querySelector("bar-main");
+
+	await BITSMIST.v1.StateOrganizer.waitFor([{"rootNode":"bar-main"}]);
+	expect(barMain.testVars["eventCalled"]["beforeRefresh"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["doRefresh"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["afterRefresh"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["beforeFetch"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["doTarget"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["doFetch"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["afterFetch"]).toBe(true);
+});
+
+// -----------------------------------------------------------------------------
+
+test('Setting Test - settings.autoFetch=false should work', async () => {
+	BarMain.prototype._getSettings = function() {
+		let settings = this.__getSettings.call(this);
+		settings["settings"]["autoFetch"] = false;
+
+		return settings;
+	};
+	document.body.innerHTML = "<bar-main></bar-main>";
+	var barMain = document.querySelector("bar-main");
+
+	await BITSMIST.v1.StateOrganizer.waitFor([{"rootNode":"bar-main"}]);
+	expect(barMain.testVars["eventCalled"]["beforeRefresh"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["doRefresh"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["afterRefresh"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["beforeFetch"]).toBe(false);
+	expect(barMain.testVars["eventCalled"]["doTarget"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["doFetch"]).toBe(false);
+	expect(barMain.testVars["eventCalled"]["afterFetch"]).toBe(false);
+});
+
+// -----------------------------------------------------------------------------
+
+test('Setting Test - settings.autoFill=true should work', async () => {
+	BarMain.prototype._getSettings = function() {
+		return this.__getSettings();
+	};
+	document.body.innerHTML = "<bar-main></bar-main>";
+	var barMain = document.querySelector("bar-main");
+
+	await BITSMIST.v1.StateOrganizer.waitFor([{"rootNode":"bar-main"}]);
+	expect(barMain.testVars["eventCalled"]["beforeFill"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["doFill"]).toBe(true);
+	expect(barMain.testVars["eventCalled"]["afterFill"]).toBe(true);
+});
+
+// -----------------------------------------------------------------------------
+
+test('Setting Test - settings.autoFill=false should work', async () => {
+	BarMain.prototype._getSettings = function() {
+		let settings = this.__getSettings.call(this);
+		settings["settings"]["autoFill"] = false;
+
+		return settings;
+	};
+	document.body.innerHTML = "<bar-main></bar-main>";
+	var barMain = document.querySelector("bar-main");
+
+	await BITSMIST.v1.StateOrganizer.waitFor([{"rootNode":"bar-main"}]);
+	expect(barMain.testVars["eventCalled"]["beforeFill"]).toBe(false);
+	expect(barMain.testVars["eventCalled"]["doFill"]).toBe(false);
+	expect(barMain.testVars["eventCalled"]["afterFill"]).toBe(false);
+});
+
+// -----------------------------------------------------------------------------
+
+test('Setting Test - settings.hasTemplate=true should work', async () => {
+	BarMain.prototype._getSettings = function() {
+		return this.__getSettings();
+	};
+	document.body.innerHTML = "<bar-main></bar-main>";
+	var barMain = document.querySelector("bar-main");
+
+	await BITSMIST.v1.StateOrganizer.waitFor([{"rootNode":"bar-main"}]);
+	expect(barMain.testVars["eventCalled"]["afterAppend"]).toBe(true);
+	expect(barMain.innerHTML).toBe("<div>bar-main</div>");
+});
+
+// -----------------------------------------------------------------------------
+
+test('Setting Test - settings.hasTemplate=false should work', async () => {
+	BarMain.prototype._getSettings = function() {
+		let settings = this.__getSettings.call(this);
+		settings["settings"]["hasTemplate"] = false;
+
+		return settings;
+	};
+	document.body.innerHTML = "<bar-main></bar-main>";
+	var barMain = document.querySelector("bar-main");
+
+	await BITSMIST.v1.StateOrganizer.waitFor([{"rootNode":"bar-main"}]);
+	expect(barMain.testVars["eventCalled"]["afterAppend"]).toBe(true);
+	expect(barMain.innerHTML).toBe("");
 });
 
 // -----------------------------------------------------------------------------
