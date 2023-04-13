595951c82e155f80c6258317abea4412885bd183
Masaki Yasutake <my17560@gmail.com>
2023-04-13 15:34:13 +0900

    Reverted.

--- src/core/component/component.js
+++ src/core/component/component.js
@@ -205,8 +205,6 @@ Component.prototype.start = function(settings)
 	}).then((newSettings) => {
 		return OrganizerOrganizer.attach(this, SettingOrganizer, {"settings":newSettings});
 	}).then(() => {
-		this._initialize();
-
 		return this.trigger("beforeStart");
 	}).then(() => {
 		console.debug(`Component.start(): Starting component. name=${this._name}, id=${this.id}, uniqueId=${this._uniqueId}`);
@@ -298,7 +296,6 @@ Component.prototype.transform = function(options)
 		}
 	}).then(() => {
 		return this.loadTags(this.rootElement);
-		//return this.organizers["ComponentOrganizer"]["methods"].loadTags(this.rootElement);
 	}).then(() => {
 		console.debug(`Component.transform(): Transformed. name=${this.name}, templateName=${templateName}, id=${this.id}, uniqueId=${this.uniqueId}`);
 		return this.trigger("afterTransform", options);
@@ -503,15 +500,6 @@ Component.prototype._getSettings = function()
 
 	return {};
 
-}
-
-// -----------------------------------------------------------------------------
-
-/**
- * Initialize component.
- */
-Component.prototype._initialize = function()
-{
 }
 
 // -----------------------------------------------------------------------------
