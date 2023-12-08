34082bdff62f44a851700fc8b49ccad89e01ef3e
Masaki Yasutake <my17560@gmail.com>
2023-12-08 18:49:12 +0900

    Refactored the export method.

---
+++ export.js
@@ -0,0 +1,20 @@
+// =============================================================================
+/**
+ * BitsmistJS - Javascript Web Client Framework
+ *
+ * @copyright		Masaki Yasutake
+ * @link			https://bitsmist.com/
+ * @license			https://github.com/bitsmist/bitsmist/blob/master/LICENSE
+ */
+// =============================================================================
+
+if (process.env.NODE_ENV === "development")
+{
+	var {Util, ClassUtil, AjaxUtil, URLUtil, Store, ChainableStore, Perk, Unit} = await import("./dist/bitsmist-js_v1.esm.js");
+}
+else
+{
+	var {Util, ClassUtil, AjaxUtil, URLUtil, Store, ChainableStore, Perk, Unit} = await import("./dist/bitsmist-js_v1.esm.min.js");
+}
+
+export {Util, ClassUtil, AjaxUtil, URLUtil, Store, ChainableStore, Perk, Unit};
