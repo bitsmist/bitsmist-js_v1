806abf36ea3ab2b2c201cd21004fb4edd4b0384a
Masaki Yasutake <my17560@gmail.com>
2023-12-08 19:00:54 +0900

    Moved export.js.

--- src/core/export.js
+++ src/core/export.js
@@ -10,11 +10,11 @@
 
 if (process.env.NODE_ENV === "development")
 {
-	var {Util, ClassUtil, AjaxUtil, URLUtil, Store, ChainableStore, Perk, Unit} = await import("./dist/bitsmist-js_v1.esm.js");
+	var {Util, ClassUtil, AjaxUtil, URLUtil, Store, ChainableStore, Perk, Unit} = await import("../../dist/bitsmist-js_v1.esm.js");
 }
 else
 {
-	var {Util, ClassUtil, AjaxUtil, URLUtil, Store, ChainableStore, Perk, Unit} = await import("./dist/bitsmist-js_v1.esm.min.js");
+	var {Util, ClassUtil, AjaxUtil, URLUtil, Store, ChainableStore, Perk, Unit} = await import("../../dist/bitsmist-js_v1.esm.min.js");
 }
 
 export {Util, ClassUtil, AjaxUtil, URLUtil, Store, ChainableStore, Perk, Unit};
