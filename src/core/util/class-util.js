e9bb78145acc02021250473d62686c9aa669ceec
Masaki Yasutake <my17560@gmail.com>
2023-12-09 11:36:16 +0900

    Modified getClass().

--- src/core/util/class-util.js
+++ src/core/util/class-util.js
@@ -88,19 +88,6 @@ export default class ClassUtil
 
 		let ret;
 
-		if (BITSMIST.V1[className])
-		{
-			ret = BITSMIST.V1[className];
-		}
-
-		if (!ret)
-		{
-			if (globalThis[className])
-			{
-				ret = globalThis[className];
-			}
-		}
-
 		if (!ret)
 		{
 			try
